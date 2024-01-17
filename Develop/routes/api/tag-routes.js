c1f0d259f989e35cf633058416d626808135175c
bsaglimb <bsaglimb>
2024-01-17 03:49:22 -0500

    added code to find all tags in the tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,9 +3,28 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+
+
+// find all tags
+// be sure to include its associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
+  })
+.then(dbTagData => res.json(dbTagData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
