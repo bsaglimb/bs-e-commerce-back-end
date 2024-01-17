89483d18d717ef398b5a5c08e210b7fbc62a3cd0
bsaglimb <bsaglimb>
2024-01-17 04:10:16 -0500

    defined columns in the category.js file

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,12 +7,22 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, // Prevent null values
+      primaryKey: true, // Set as primary key
+      autoIncrement: true // Uses auto increment
+    },
+    category_name: {
+      type: DataTypes.STRING, // Uses string data type
+      allowNull: false
+    }
   },
   {
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
+    timestamps: false, // Don't automatically create createdAt/updatedAt timestamp fields
+    freezeTableName: true, // Don't pluralize name of database table
+    underscored: true, // Use underscores instead of camel-casing
     modelName: 'category',
   }
 );
