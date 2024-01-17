6697efa68331051808f07e98c8beb84de7935f7a
bsaglimb <bsaglimb>
2024-01-17 04:14:51 -0500

    added code to the index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // Set custom foreign key
+});
+
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+
+
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
+
+
 
 module.exports = {
   Product,
